f4a9bd80d66625767d649b923280e154a3301b85
Zeliwipin <zeliwipin@gmail.com>
2022-03-28 10:52:05 +0200

    added allowed host

--- diary/settings.py
+++ diary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$%7ympr@)h$90@l0k1r(-ctx$iz@o(yqlx%lg2d_kv73*2xp=#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://django-blog-app-boris.herokuapp.com/']
 
 
 # Application definition
